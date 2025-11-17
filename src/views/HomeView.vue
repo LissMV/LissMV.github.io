ae928dc492bc4fefb30d6a1f21f03a15c022c545
LissMV <valencialisseth47@gmail.com>
2025-11-16 22:09:11 -0600

    installed colors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-violeta">Este es un coso de prueba, funciona</p>
+  <p class="text-cyan">Este es un coso de prueba, funciona</p>
   <h1 class="text-red-600">No creo</h1>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,4 @@ export default {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        turquesa: '#2AC984',
-        cyan: '#338B85',
-        gris_azul: '#9AB1AF',
-        gris_claro: '#CDCACA',
-        aqua_claro: '#54C3BB',
-        violeta: '#6B2188',
-      },
-    },
-  },
-  plugins: [],
 }
