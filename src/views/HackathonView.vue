db3c95e184fc533655cd74ae3e8650f548130ca0
LissMV <valencialisseth47@gmail.com>
2025-11-23 23:32:19 -0600

    hackathon view

--- src/views/HackathonView.vue
+++ src/views/HackathonView.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="flex justify-center">
-    <h1 class="text-2xl text-white">Esto es Hackathon view</h1>
+  <div class="flex justify-center mt-[82px] ml-20">
+    <h1
+      class="font-harys text-[65px] font-bold bg-gradient-to-b from-aqua-claro to-violeta to-[100%] bg-clip-text text-transparent">
+      Hackathon 2023</h1>
+  </div>
+  <div class="pl-[300px] pr-[190px] mt-5">
+    <h1 class="font-afacad text-white text-[23px] leading-[31px] text-justify">
+      This project was developed as a team during the first year of the ¡Supérate! scholarship. which consisted of developing a solution to a problem.
+      This project was worked on using Office 365 tools, especially Power Apps and Microsoft Lists, among others. The goal was to demonstrate the lessons
+      we had learned throughout the year. It was the first project focused on technology and the creation of an idea. It was like learning a small part
+      of EXPO and an experience prior to that.</h1>
+      <h1 class="text-white font-abhaya text-[30px] mt-10">My role</h1>
+    <h1 class="font-afacad text-white text-[23px] leading-[31px] text-justify mt-4">
+      My role in this project was to be the leader. I learned things such as teamwork, organization, values, and assertive communication. My contribution to
+      the project was to create the database in Microsoft Lists and connect it to Power Apps, as well as helping to create the logo. </h1>
+      <h1
+      class="font-harys text-[65px] font-bold bg-gradient-to-b from-aqua-claro to-violeta to-[100%] bg-clip-text text-transparent mt-5">
+      Star Cars</h1>
+      <h1 class="font-afacad text-white text-[23px] leading-[31px] text-justify mt-4">
+       The objective of this project is to facilitate the purchase and rental of used cars in El Salvador. Provide your role (buyer, personal business, individual
+       seller) and access the application.
+      </h1>
+      <h1 class="text-white font-abhaya text-[30px] mt-10 text-center">Power Apps desing</h1>
+
+      <section class="flex items-center justify-center space-x-4 mt-8">
+        <img src="@/assets/img/hackathon/desing1.png" class="w-[281px] h-[158px]">
+        <img src="@/assets/img/hackathon/desing2.png" class="w-[281px] h-[158px]">
+        <img src="@/assets/img/hackathon/desing3.png" class="w-[281px] h-[158px]">
+      </section>
+       <section class="flex items-center justify-center space-x-4 mt-4">
+        <img src="@/assets/img/hackathon/desing4.png" class="w-[281px] h-[158px]">
+        <img src="@/assets/img/hackathon/desing5.png" class="w-[281px] h-[158px]">
+        <img src="@/assets/img/hackathon/desing6.png" class="w-[281px] h-[158px]">
+      </section>
+       <section class="flex items-center justify-center space-x-4 mt-4">
+        <img src="@/assets/img/hackathon/desing7.png" class="w-[281px] h-[158px]">
+      </section>
+
+      <h1 class="text-white font-abhaya text-[30px] mt-10 text-center">Data Base in Lists</h1>
+
+      <section class="flex items-center justify-center space-x-4 mt-8">
+        <img src="@/assets/img/hackathon/db1.png" class="w-[424px] h-[199px]">
+        <img src="@/assets/img/hackathon/db2.png" class="w-[424px] h-[199px]">
+      </section>
+       <section class="flex items-center justify-center space-x-4 mt-4 mb-6">
+        <img src="@/assets/img/hackathon/db3.png" class="w-[424px] h-[199px]">
+      </section>
   </div>
 </template>
