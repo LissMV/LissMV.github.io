15666c24f771621fa2d6c60347046c1de9eca475
LissMV <valencialisseth47@gmail.com>
2025-11-24 09:53:04 -0600

    routes

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
-  base: 'lissethmejiava.github.io',
+  base: '/',   
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
