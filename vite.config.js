aa2d71b4d53ccdf8029f644f96d43fbbd2a04dbc
LissMV <valencialisseth47@gmail.com>
2025-11-24 10:56:31 -0600

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/LissMV.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
